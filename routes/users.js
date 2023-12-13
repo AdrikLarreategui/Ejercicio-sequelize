9f5c64e58ef3d61f9a32ae8298a69968658ca994
AdrikLarreategui <adrian15186@hotmail.com>
2023-12-13 19:51:05 +0100

    commit

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require ('express')
 const router = express.Router()
-const UserController = require('../controllers/UserController.js')
+const UserController = require('../controllers/userController.js')
 router.post('/', UserController.create)
 module.exports = router
 
