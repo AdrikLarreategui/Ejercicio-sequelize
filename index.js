81f1a8b33dd0aecab4602370ff7412174041f020
AdrikLarreategui <adrian15186@hotmail.com>
2023-12-13 20:22:08 +0100

    commit seeders creadas

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const PORT = 3000
 
 app.use(express, json())
 app.use('/users', require('./routes/users'))
-//Pendiente hacer unión entre product product route, etc.
+
 
 app.listen(PORT, ()=> console.log(`Server created at port ${PORT}`))
