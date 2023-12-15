3cf7211222b138c1bb571a3c65bf75fd02bcfa58
AdrikLarreategui <adrian15186@hotmail.com>
2023-12-15 21:32:25 +0100

    commit userRoutes actualizado

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const UserController = require('../controllers/UserController.js')
-const { authentication } = require('../middlewares/authentication')
+const { authentication } = require('../middleware/authentication.js')
 
-router.post ('/register', UserController.create)
+router.get ('/getAllUsers', authentication, UserController.findAll)
+router.post('/register', UserController.create)
 router.post ('/login', UserController.login)
 router.delete('/delete/:id', authentication, UserController.deleteUSer)
+router.delete('/logout', authentication, UserController.logout)
 
 modules.exports = router
